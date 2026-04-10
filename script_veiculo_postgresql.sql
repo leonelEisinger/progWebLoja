9595584311161e7834be28a62ba3cbf875316f52
Leonel Eisinger <42424255+leonelEisinger@users.noreply.github.com>
2026-04-09 21:04:54 -0300

    Expand database schema with new tables and constraints

--- script_veiculo_postgresql.sql
+++ script_veiculo_postgresql.sql
@@ -1,35 +1,38 @@
-create table usuario (
-id serial not null,
-login varchar(30) not null unique,
-senha varchar(255) not null,
-nome varchar(255) not null);
+create table cliente (
+	id serial not null,
+	login varchar(30) not null unique,
+	senha varchar(255) not null,
+	nome varchar(255) not null,
+	telefone varchar(13) not null,
+	email varchar(100) not null,
+	cartaoCredito varchar(16) not null
 
-alter table usuario 
-add constraint pk_usuario
-primary key(id);
+	primary key(id)
+);
 
-insert into usuario(login, senha, nome) values ('krohn','123','Alexandre Krohn');
-insert into usuario(login, senha, nome) values ('alexandre','123','Alexandre K.');
-insert into usuario(login, senha, nome) values ('god','123','God account');
+create table endereco (
+	id serial not null,
+	rua varchar(255),
+	numero varchar(5),
+	complemento varchar(15),
+	bairro varchar(255),
+	cep varchar(8),
+	cidade varchar(255),
+	estado varchar(255),
 
-insert into usuario(login, senha, nome) values ('joao','123','João da Silva');
-insert into usuario(login, senha, nome) values ('maria','123','Maria da Silva');
-insert into usuario(login, senha, nome) values ('megan','123','Megan da Silva');
+	primary key(id)
+);
 
-
-create table marca (
+create table fornecedor (
 id serial not null,
 nome varchar(60) not null,
 descricao varchar(255),
+telefone varchar(13) not null,
+email varchar(100),
 primary key(id)
 );
 
-insert into marca(nome, descricao) values ('volkswagen','Das Auto');
-insert into marca(nome, descricao) values ('chevrolet','Find New Roads');
-insert into marca(nome, descricao) values ('bmw','Sheer Driving Pleasure');
-insert into marca(nome, descricao) values ('Lamborghini','Dare to live more');
-
-create table veiculo (
+create table produto (
 id serial not null,
 nome varchar(60) not null,
 placa char(7) not null,
@@ -38,16 +41,37 @@ marca_id int,
 primary key(id)
 );
 
-insert into veiculo(nome, placa, cor, marca_id) values ('vectra','abc1234','cinza', 2);
-insert into veiculo(nome, placa, cor, marca_id) values ('gol','dfg7532','preto', 1);
-insert into veiculo(nome, placa, cor, marca_id) values ('328i','ert8412','branco', 3);
-insert into veiculo(nome, placa, cor, marca_id) values ('venom','cgr146','laranja', 4);
+create table estoque (
+	id serial not null,
+	id_produto int not null,
+	qtd int,
+	preco double precision,
+	
+	primary key(id)
+	foreign key (produto_id) references produto(id)
+)
+
+create table pedido (
+	numero serial not null,
+	dataPedido date not null,
+	dataEntrega date not null,
+	situacao varchar(10) not null,
+	primary key(id),
+);
 
 
-alter table veiculo
-add constraint fk_veiculo_marca
-foreign key (marca_id)
-references marca(id); 
+create table itemPedido(
+ id serial not null,
+ pedido_id int,
+ produto_id int,
+ quantidade int,
+ preco double precision,
+
+ foreign key (pedido_id) references pedido(id),
+ foreign key (produto_id) references produto(id)
+);
+
+
 
 select *
 from usuario;
@@ -58,6 +82,9 @@ from marca;
 select *
 from veiculo;
 
+insert into cliente(login, senha, nome) values ('krohn','123','Alexandre Krohn');
+insert into cliente(login, senha, nome) values ('alexandre','123','Alexandre K.');
+insert into cliente(login, senha, nome) values ('god','123','God account');
 
 
 
