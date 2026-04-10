c724d4c9b9dd7c9c9f33fef5739ff1db60bdcd5e
Leonel Eisinger <42424255+leonelEisinger@users.noreply.github.com>
2026-04-09 21:39:18 -0300

    inserts
    Updated SQL script to include additional fields in the tables and added insert statements for new records.

--- script_veiculo_postgresql.sql
+++ script_veiculo_postgresql.sql
@@ -13,6 +13,9 @@ create table cliente (
 select *
 from cliente;
 
+insert into cliente(login, senha, nome, telefone, email, cartaoCredito) values ('krohn','123','Alexandre Krohn', '5554999999999', 'email@email.com', '1234567890654321');
+insert into cliente(login, senha, nome, telefone, email, cartaoCredito) values ('teste','321','Teste da Silva', '5252988889999', 'testesilva@email.com', '1597532684159753');
+
 create table endereco (
 	id serial not null,
 	rua varchar(255),
@@ -29,6 +32,9 @@ create table endereco (
 select *
 from endereco;
 
+insert into endereco(rua, numero, complemento, bairro, cep, cidade, estado) values ('Rua principal', '12345', 'Universidade', 'UCS', '65000100', 'Caxias do sul', 'RS');
+insert into endereco(rua, numero, complemento, bairro, cep, cidade, estado) values ('Teste', '999', 'casa', 'Pio x', '11111111', 'Caxias do sul', 'RS');
+
 create table fornecedor (
 	id serial not null,
 	nome varchar(60) not null,
@@ -42,18 +48,23 @@ create table fornecedor (
 select *
 from fornecedor;
 
+insert into fornecedor(nome, descricao, telefone, email) values ('fornceAlgo', 'Fornecedor que fornece algo', '5554999889988', 'fornecealgo@email.com.br');
+insert into fornecedor(nome, descricao, telefone, email) values ('entregaAlgo', 'Entrega algo', '5150955775577', 'entregaAlgo@email.com.br');
+
 create table produto (
 	id serial not null,
 	nome varchar(60) not null,
-	placa char(7) not null,
-	cor varchar(20) not null,
-	marca_id int not null,
+	descricao varchar(255) not null,
+	foto varchar(255) not null,
 	
 	primary key(id)
 );
 
 select *
 from produto;
+
+insert into produto(nome, descricao, foto) values ('Chave', 'Abre algo', '');
+insert into produto(nome, descricao, foto) values ('Roda', 'Apenas Gira', '');
 
 create table estoque (
 	id serial not null,
@@ -68,6 +79,9 @@ create table estoque (
 select *
 from estoque;
 
+insert into estoque(produto_id, qtd, preco) values (1, 3, 25.0);
+insert into estoque(produto_id, qtd, preco) values (2, 1, 150.0);
+
 create table pedido (
 	numero serial not null,
 	dataPedido date not null,
@@ -78,6 +92,9 @@ create table pedido (
 
 select *
 from pedido;
+
+insert into pedido(dataPedido, dataEntrega, situacao) values('20/02/2019','25/02/2019','ENTREGUE');
+insert into pedido(dataPedido, dataEntrega, situacao) values('31/08/2030','25/02/2030','CANCELADO');
 
 create table itemPedido(
  id serial not null,
@@ -90,15 +107,10 @@ create table itemPedido(
  foreign key (produto_id) references produto(id)
 );
 
-
 select *
 from itemPedido;
 
-
-
-insert into cliente(login, senha, nome) values ('krohn','123','Alexandre Krohn');
-insert into cliente(login, senha, nome) values ('alexandre','123','Alexandre K.');
-insert into cliente(login, senha, nome) values ('god','123','God account');
-
+insert into itemPedido(pedido_id, produto_id, quantidade, preco) values(1,2,1, 160.0);
+insert into itemPedido(pedido_id, produto_id, quantidade, preco) values(2,1,3, 100.0);
 
 
